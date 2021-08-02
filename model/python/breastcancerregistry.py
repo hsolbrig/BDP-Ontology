79651d29911703e8eddb7ce3f65f25e01e1b9385
hsolbrig <solbrig@jhu.edu>
2021-08-02 15:51:10 -0500

    Add first cut at the breast cancer registry ontology and the registry bridge ontology.
    LinkML didn't come close to generating what we needed, so this was created with Protege.  We've encountered a number of interesting issues:
    1) "id" was intended to reference the person being diagnosed.  At the moment, the YAML is incorrect, as we have stated that it is the
    identifier of the BCR record itself.  We will fix this
    2) "entryDate" should map to a _predicate_ that identifies the date that the DX occurred.  Curiously, there is a _class_, "date of diagnosis" (https://www.ebi.ac.uk/ols/ontologies/efo/terms?short_form=EFO_0004953) that sort of identifies what our predicate is, but I have NO idea how we'd represent an actual date in this situation.  We need to ask the EFO folks.
    
    Also note that we attempted to import MONDO for the breast cancer diagnosis -- the URL that we got from OLS didn't work because it had a formatting error.  We picked up a URL from BioPortal but, after 20 minutes, it was still loading so we gave up and moved to HPO

---
+++ model/python/breastcancerregistry.py
@@ -0,0 +1,82 @@
+# Auto generated from BreastCancerRegistry.yaml by pythongen.py version: 0.9.0
+# Generation date: 2021-08-02 12:29
+# Schema: bcr
+#
+# id: http://example.org/bdpontology/bcr
+# description:
+# license: https://creativecommons.org/publicdomain/zero/1.0/
+
+import dataclasses
+import sys
+import re
+from jsonasobj2 import JsonObj
+from typing import Optional, List, Union, Dict, ClassVar, Any
+from dataclasses import dataclass
+from linkml_runtime.linkml_model.meta import EnumDefinition, PermissibleValue, PvFormulaOptions
+
+from linkml_runtime.utils.slot import Slot
+from linkml_runtime.utils.metamodelcore import empty_list, empty_dict, bnode
+from linkml_runtime.utils.yamlutils import YAMLRoot, extended_str, extended_float, extended_int
+from linkml_runtime.utils.dataclass_extensions_376 import dataclasses_init_fn_with_kwargs
+from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
+from linkml_runtime.utils.enumerations import EnumDefinitionImpl
+from rdflib import Namespace, URIRef
+from linkml_runtime.utils.curienamespace import CurieNamespace
+from linkml_model.types import Date, Integer, String
+from linkml_runtime.utils.metamodelcore import XSDDate
+
+metamodel_version = "1.7.0"
+
+# Overwrite dataclasses _init_fn to add **kwargs in __init__
+dataclasses._init_fn = dataclasses_init_fn_with_kwargs
+
+# Namespaces
+BCR = CurieNamespace('bcr', 'https://example.org/bdpontology/bcr/')
+LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
+XSD = CurieNamespace('xsd', 'http://www.w3.org/2001/XMLSchema#')
+DEFAULT_ = BCR
+
+
+# Types
+
+# Class references
+class EntryId(extended_str):
+    pass
+
+
+@dataclass
+class Entry(YAMLRoot):
+    """
+    An entry in a breast cancer registry
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = BCR.Entry
+    class_class_curie: ClassVar[str] = "bcr:Entry"
+    class_name: ClassVar[str] = "Entry"
+    class_model_uri: ClassVar[URIRef] = BCR.Entry
+
+    id: Union[str, EntryId] = None
+    entryDate: Optional[Union[str, XSDDate]] = None
+    age: Optional[int] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            raise ValueError("id must be supplied")
+        if not isinstance(self.id, EntryId):
+            self.id = EntryId(self.id)
+
+        if self.entryDate is not None and not isinstance(self.entryDate, XSDDate):
+            self.entryDate = XSDDate(self.entryDate)
+
+        if self.age is not None and not isinstance(self.age, int):
+            self.age = int(self.age)
+
+        super().__post_init__(**kwargs)
+
+
+# Enumerations
+
+
+# Slots
+
